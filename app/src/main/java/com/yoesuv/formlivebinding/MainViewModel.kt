6206bcd0bd6069b1e89df12918603a1e6e2db391
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-05-03 20:57:50 +0700

    :pencil: set button enabled or disabled

--- app/src/main/java/com/yoesuv/formlivebinding/MainViewModel.kt
+++ app/src/main/java/com/yoesuv/formlivebinding/MainViewModel.kt
@@ -13,19 +13,31 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     var email: MutableLiveData<String> = MutableLiveData()
     var password: MutableLiveData<String> = MutableLiveData()
+    var isValid: MutableLiveData<Boolean> = MutableLiveData()
 
     var errorEmail: MutableLiveData<String> = MutableLiveData()
     var errorPassword: MutableLiveData<String> = MutableLiveData()
 
+    private var isEmailValid: Boolean = false
+    private var isPasswordValid: Boolean = false
+
     fun setupLiveData(lifecycleOwner: LifecycleOwner, context: Context) {
         email.observe(lifecycleOwner, Observer { email ->
             val validationModel = email.validateEmail(context)
+            isEmailValid = validationModel.isValid
+            validateInput(isEmailValid, isPasswordValid)
             errorEmail.postValue(validationModel.message)
         })
         password.observe(lifecycleOwner, Observer { password ->
             val validationModel = password.validatePassword(context)
+            isPasswordValid = validationModel.isValid
+            validateInput(isEmailValid, isPasswordValid)
             errorPassword.postValue(validationModel.message)
         })
+    }
+
+    private fun validateInput(email: Boolean, password: Boolean) {
+        isValid.postValue(email && password)
     }
 
 }
