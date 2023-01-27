a7ae9b0d43541fce040c279f63470ddee0e71b18
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-01-27 10:06:38 +0700

    :tractor: add ui automator

--- app/src/androidTest/java/com/yoesuv/formlivebinding/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/yoesuv/formlivebinding/ExampleInstrumentedTest.kt
@@ -2,6 +2,7 @@ package com.yoesuv.formlivebinding
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.uiautomator.UiDevice
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,6 +16,9 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    private val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
