46e93ff3b45ca21a75b2af5e759ad4038bd5b2ad
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-01-27 17:17:46 +0700

    :pencil: test error input email

--- app/src/androidTest/java/com/yoesuv/formlivebinding/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/yoesuv/formlivebinding/ExampleInstrumentedTest.kt
@@ -1,12 +1,13 @@
 package com.yoesuv.formlivebinding
 
+import android.content.Context
+import android.os.SystemClock
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.clearText
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.uiautomator.UiDevice
@@ -16,7 +17,6 @@ import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 import org.junit.Before
-import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -27,9 +27,12 @@ import org.junit.Rule
 class ExampleInstrumentedTest {
 
     private val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+    private val delay = 1000L
+    private lateinit var context: Context
 
     @Before
     fun register() {
+        context = InstrumentationRegistry.getInstrumentation().targetContext
         ActivityScenario.launch(MainActivity::class.java)
     }
 
@@ -37,9 +40,14 @@ class ExampleInstrumentedTest {
     fun startFlowPositive() {
         onView(withId(R.id.etEmail)).perform(typeText("apple@gmail.com"))
             .check(matches(withText("apple@gmail.com")))
+        SystemClock.sleep(delay)
         onView(withId(R.id.etEmail)).perform(clearText())
-        onView(withId(R.id.textInputEmail)).check(matches(hasErrorText("Email is empty")))
-        //onView(withId(R.id.etPassword)).perform(typeText("leMinerale"))
+        onView(withText(context.getString(R.string.validation_email_empty))).check(matches(isDisplayed()))
+        SystemClock.sleep(delay)
+        onView(withId(R.id.etEmail)).perform(typeText("apple@gmail.com."))
+        SystemClock.sleep(delay)
+        onView(withText(context.getString(R.string.validation_email_not_valid))).check(matches(isDisplayed()))
+        SystemClock.sleep(delay)
 
     }
 
