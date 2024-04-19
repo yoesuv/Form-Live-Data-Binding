4c31a46f053b0e463ad2fd93cb73607ff350b5c3
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-04-19 21:01:51 +0700

    :tractor: build kotlin dsl

--- app/src/main/java/com/yoesuv/formlivebinding/MainActivity.kt
+++ app/src/main/java/com/yoesuv/formlivebinding/MainActivity.kt
@@ -1,4 +1,4 @@
- package com.yoesuv.formlivebinding
+package com.yoesuv.formlivebinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -6,20 +6,20 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.yoesuv.formlivebinding.databinding.ActivityMainBinding
 
- class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
-     private lateinit var binding: ActivityMainBinding
-     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
 
-     override fun onCreate(savedInstanceState: Bundle?) {
-         super.onCreate(savedInstanceState)
-         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-         binding.lifecycleOwner = this
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
 
-         setSupportActionBar(binding.toolbar)
+        setSupportActionBar(binding.toolbar)
 
-         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-         binding.main = viewModel
-         viewModel.setupLiveData(this, this)
-     }
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        binding.main = viewModel
+        viewModel.setupLiveData(this, this)
+    }
 }
