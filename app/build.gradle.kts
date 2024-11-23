f8a0fc2fae13d7f2ae0d5ce4dc7958e4b0f64576
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-11-23 15:53:52 +0700

    :pencil: update link apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 8
-        versionName = "1.1.2"
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         setProperty("archivesBaseName", "$applicationId-v$versionCode($versionName)")
